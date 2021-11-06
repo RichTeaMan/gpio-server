f482a3014b4548af979d7f5076cacc402e2aee63
Thomas Holmes <thomasholmes@outlook.com>
2021-11-06 14:01:22 +0000

    Added flask.

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,9 @@ FROM python:3-buster
 WORKDIR /app
 
 RUN python -m pip install --upgrade pip
-RUN pip3 install RPi.GPIO gpiozero
+RUN pip3 install RPi.GPIO gpiozero flask-restful
 
 COPY gpio.py gpio.py
+COPY api.py api.py
 
-ENTRYPOINT python3 gpio.py
+ENTRYPOINT python3 api.py
---
+++ api.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from gpiozero import LED
+
+leds = {}
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+class PinTest(Resource):
+    def get(self):
+        if (4 not in leds):
+            leds[4] = LED(4)        
+        leds[4].toggle()
+        return {'done': 'maybe'}
+
+class Gpio(Resource):
+    def get(self, gpio_id):
+        print(gpio_id)
+        print(type(gpio_id))
+        if (gpio_id not in leds):
+            leds[gpio_id] = LED(gpio_id)   
+        leds[gpio_id].toggle()        
+        return {gpio_id: leds[gpio_id].is_active}
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(PinTest, '/pin')
+api.add_resource(Gpio, '/gpio/<int:gpio_id>/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
